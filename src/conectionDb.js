da0a5291d7fd6da82cb95c5a7dc4f158de97b657
Bruja <sebaspe12345@gmail.com>
2020-03-25 15:43:05 -0500

    x2

--- src/conectionDb.js
+++ src/conectionDb.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose')
 
 
-mongoose.connect('mongodb://localhost:27017/Images')
+/*mongoose.connect('mongodb://localhost:27017/Images')
     .then(connect => { console.log('db id connected') })
-    .catch(err => { console.error(err) })
+    .catch(err => { console.error(err) })*/
 
-/*mongoose.connect('mongodb+srv://Bruja:1925BscJp@cluster0-hp4pe.mongodb.net/Images?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://Bruja:1925BscJp@cluster0-hp4pe.mongodb.net/Images?retryWrites=true&w=majority',{
     useNewUrlParser:true,
     useUnifiedTopology:true
 })
     .then(db => console.log('conected to Mongo'))
     .catch(err => console.error(err));
-*/
+
 module.exports=mongoose
